7cc27f5484631c600e8764f5ea3e36acb07154b0
Alfredo Oltra <aoltra@gmail.com>
2023-01-22 23:59:49 +0100

    [feat] Parámetros configuración aules

--- models/config_settings.py
+++ models/config_settings.py
@@ -4,5 +4,5 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    aules_user = fields.Char(string = "Usuario acceso a Aules")
-    password_user = fields.Char(string = "Password acceso a Aules")
+    aules_user = fields.Char(string = 'Usuario acceso a Aules', config_parameter='atenea.aules_user')
+    aules_password = fields.Char(string = 'Password acceso a Aules', config_parameter='atenea.aules_password')
