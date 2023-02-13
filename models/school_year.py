e9f297c11cdc9148fd26de66d8330ebd3a5981b5
Alfredo Oltra <aoltra@gmail.com>
2023-02-13 01:35:54 +0100

    feat(core): se añade inicio segunda evaluación

--- models/school_year.py
+++ models/school_year.py
@@ -25,18 +25,27 @@ class SchoolYear(models.Model):
   date_init_lective = fields.Date(string = 'Fecha de inicio real', compute = '_compute_date_init_lective', readonly = False, store = True)
   # jornadas de bienvenida
   date_welcome_day = fields.Date(string = 'Jornadas de bienvenida', compute = '_compute_welcome_day', store = True)
+  # SEGUNDO
   # fin de las clases de la primera evaluación de segundo
   date_1term2_end = fields.Date(string = 'Fin clases primera evaluación', compute = '_compute_1term2_end', readonly = False, store = True) 
   # inicio examenes 1 evaluación de segundo. En caso de readonly True hay que forzar su grabación en el XML con force_save
   date_1term2_exam_ini = fields.Date(string = 'Inicio exámenes primera evaluación', compute = '_compute_1term2_exam_ini', readonly = False, store = True) 
   # fin exámenes 1 evaluación de segundo
   date_1term2_exam_end = fields.Date(string = 'Fin exámenes primera evaluación', compute = '_compute_1term2_exam_end', store = True) 
+  # duración primera evaluación segundo
+  duration_1term2 = fields.Integer(string = 'Duración (semanas)', compute = '_compute_duration_1term2')
+  # inicio segunda evaluación
+  date_2term2_ini = fields.Date(string = 'Inicio segunda evaluación', compute = '_compute_2term2_ini', store = True) 
+
 
   holidays_ids = fields.One2many('atenea.holiday', 'school_year_id')
 
   # report calendario escolar  
   school_calendar_version = fields.Integer(string = 'Versión calendario escolar', default = 1, store = True, readonly = True)
-  school_calendar_update_keys = ['init_lective', 'date_init_lective', 'date_welcome_day', 'date_1term2_end', 'date_1term2_exam_ini', 'date_1term2_exam_end']
+  school_calendar_update_keys = ['init_lective', 'date_init_lective', 'date_welcome_day', 'date_1term2_end', 
+    'date_1term2_exam_ini', 'date_1term2_exam_end', 'date_2term2_ini']
+
+  # TODO: constraints para que se mantenga el orden cronológico de las fechas
 
   """ Sobreescritura de la función create que es llamada cuando se crea un registro nuevo """
   @api.model
@@ -100,6 +109,14 @@ class SchoolYear(models.Model):
     for record in self:
       if record.date_init.weekday() == 5 or record.date_init.weekday() == 6:
         raise ValidationError('La fecha de fin de evaluación no puede ser fin de semana')
+
+  @api.depends('date_1term2_end','date_init_lective')    
+  def _compute_duration_1term2(self):
+    for record in self:
+      if record.date_1term2_end != False and record.date_init_lective != False:
+        record.duration_1term2 = (record.date_1term2_end - record.date_init_lective).days // 7 + 1
+      else:
+        record.duration_1term2 = 0
              
   @api.depends('date_1term2_end', 'holidays_ids.date')
   def _compute_1term2_exam_ini(self):
@@ -114,7 +131,7 @@ class SchoolYear(models.Model):
       inma_holiday = next((holiday for holiday in record.holidays_ids if holiday.key == 'inmaculada'), None)
    
       if constitucion_holiday == None or inma_holiday == None:
-        record.date_1term2_exam_ini = ''
+        continue
       # si en la semana de examenes está el día 6/12 o el 8/12, retraso los examenes una semana
       elif (constitucion_holiday.date >= record.date_1term2_exam_ini and constitucion_holiday.date < record.date_1term2_exam_end) or \
         (inma_holiday.date >= record.date_1term2_exam_ini and inma_holiday.date < record.date_1term2_exam_end):
@@ -122,10 +139,10 @@ class SchoolYear(models.Model):
         record.date_1term2_exam_end = record.date_1term2_exam_end + datetime.timedelta(weeks = 1)
 
   @api.constrains('date_1term2_exam_ini')
-  def _check_date_1term2_end(self):
+  def _check_date_1term2_exam_ini(self):
     for record in self:
-      if record.date_init.weekday() != 0:
-        raise ValidationError('La fecha de inicio de examenes tiene que ser un lunes')
+      if record.date_1term2_exam_ini.weekday() != 0:
+        raise ValidationError('La fecha de inicio de exámenes tiene que ser un lunes')
   
   @api.depends('date_1term2_exam_ini')
   def _compute_1term2_exam_end(self):
@@ -141,10 +158,21 @@ class SchoolYear(models.Model):
       if record.date_init_lective == False:
         record.date_welcome_day = ''
       else: 
-        record.date_welcome_day = record.date_init_lective - datetime.timedelta(days=4)
+        record.date_welcome_day = record.date_init_lective - datetime.timedelta(days = 4)
 
+  @api.depends('date_1term2_exam_end')
+  def _compute_2term2_ini(self):
+    for record in self:
+      if record.date_1term2_exam_end == False:
+        record.date_2term2_ini = ''
+      else: 
+        record.date_2term2_ini = record.date_1term2_exam_end + datetime.timedelta(days = 3)
+    
   @api.onchange('date_init')
   def _calculate_holidays(self):
+    if self._origin.date_init == False:
+      return
+    
     for record in self:
       # si el año anterior es igual al que se acaba de cambiar no se hace nada
       if self._origin.date_init.year == record.date_init.year:
@@ -176,7 +204,6 @@ class SchoolYear(models.Model):
       date_fallas_holidayE = datetime.datetime(record.date_init.year + 1, 3, 19) # de partida es el 19/3
       if date_fallas_holidayE.weekday() >= 3 and date_fallas_holidayE.weekday() <= 5:
         date_fallas_holidayE = date_fallas_holidayE + datetime.timedelta(days = 6 - date_fallas_holidayE.weekday())
-
 
       # elimino todos los registros "en el aire", pero se recuperan en el caso de que se cancele la modificación
       # del school_year
@@ -320,7 +347,13 @@ class SchoolYear(models.Model):
     }
 
     self.dates['date_welcome_day'] = { 
-    'date': self.date_welcome_day, 
-    'desc': self._fields['date_welcome_day'].string,
-    'type': 'G'
-  }
+      'date': self.date_welcome_day, 
+      'desc': self._fields['date_welcome_day'].string,
+      'type': 'G'
+    }
+    
+    self.dates['date_2term2_ini'] = { 
+      'date': self.date_2term2_ini,
+      'desc': self._fields['date_2term2_ini'].string, 
+      'type': 'S',
+    }
