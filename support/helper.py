9e29c7beeba3169e8941cf7678ad558a10fbce0d
Alfredo Oltra <aoltra@gmail.com>
2023-03-30 12:08:37 +0200

    feat(core): set y unset flag

--- support/helper.py
+++ support/helper.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
-def is_set(value, option):
+def is_set_flag(value, option):
     """
     Devuelve True o False si la opción aparece en el valor
-    Por ejemplo: is_set(25, 1) -> True
-    Por ejemplo: is_set(25, 2) -> False
+    Por ejemplo: is_set_flag(25, 1) -> True
+    Por ejemplo: is_set_flag(25, 2) -> False
     """
     return value & 1 << option != 0
+
+def set_flag(value, option):
+    """
+    Asigna un uno (pone a True) en la propiedad (posición) indicada 
+    Por ejemplo: set_flag(24, 1) -> 25
+    """
+    return value | (1 << option) 
+
+def unset_flag(value, option):
+    """
+    Asigna un cero (pone a False) en la propiedad (posición) indicada 
+    Por ejemplo: unset_flag(25, 1) -> 24
+    """
+    return value | ~(1 << option) 
