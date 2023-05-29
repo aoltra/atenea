03a03264181981ab8b4d5cebbdba37319e19bc66
Alfredo Oltra <aoltra@gmail.com>
2023-05-29 21:35:10 +0000

    feat(valid): control de errores en la actualización de validation_subject (write)
    Se incluye un nuevo estado "Por revisar"

--- models/validation_subject.py
+++ models/validation_subject.py
@@ -53,15 +53,13 @@ class ValidationSubject(models.Model):
       help = "Permite indicar el motivo por el que acepta o deniega la convalidación")
   
   correction_reason = fields.Selection([
-    ('---', ''),   # no hay pendiente ninguna subsanación
     ('ANC', 'Anexo no cumplimentado correctamente. Campos obligatorios no rellenados.'),
     ('ANP', 'Anexo no cumplimentado correctamente. Tipo (convalidación/aprobado con anterioridad) no indicado.'),
     ('SNF', 'Documento no firmado digitalmente'),
     ('RL', 'No se aporta curso de riesgo laborales > 30h'),
     ('EXP', 'No se aporta expediente académico'),
-    ], string ='Razón de la subsanación', default = '---',
+    ], string ='Razón de la subsanación',
     help = "Permite indicar el motivo por el que se solicita la subsanación")
-
 
   """  # la nota tiene que estar entre 5 (tiene que esta aprobado) y 11
   @api.constrains('mark')
@@ -85,22 +83,78 @@ class ValidationSubject(models.Model):
                ('2', 'Instancia superior'),
                ('3', 'Resuelta'),    
                ('4', 'Revisada'),
-               ('5', 'Finalizada')]
+               ('5', 'Por revisar'), # desde secretaria ven un error y la tiran para atrás
+               ('6', 'Finalizada')]
     
     # if ordenados por orden de grupos de más importante a menos
     if self.env.user.has_group('atenea.group_ROOT'): # root todas las opciones
       return choices
     
-    if self.env.user.has_group('atenea.group_MNGT_FP'): # coordinación de FP todas menos finalizar
-      del choices[-1:]
-    elif self.env.user.has_group('atenea.group_VALID'): # convalidadores, todas menos las dos últimas
+    if self.env.user.has_group('atenea.group_MNGT_FP'): # coordinación de FP todas menos finalizar y por revisar
       del choices[-2:]
-    elif self.env.user.has_group('atenea.group_ADMIN'): # Secretaria
-      del choices[:-2]
+    elif self.env.user.has_group('atenea.group_VALID'): # convalidadores, todas menos las tres últimas
+      del choices[-3:]
+    elif self.env.user.has_group('atenea.group_ADMIN'): # Secretaria sólo las tres últimas
+      del choices[:-3]
     else: # cualquier otro grupo no tiene opciones
       choices.clear()
 
-    return choices
+    return choices 
+  
+  def _check_attribute_value(self, field_name, vals) -> bool:
+    if isinstance(self._fields[field_name], fields.Char):
+      return ((field_name in vals and vals[field_name] == '') or \
+              (field_name not in vals and self[field_name] == ''))
+    
+    if isinstance(self._fields[field_name], fields.Selection):
+      return ((field_name in vals and vals[field_name] == False) or \
+              (field_name not in vals and self[field_name] == False))
+    
+    return False
+ 
+  def write(self, vals):
+    """
+    Actualiza en la base de datos un registro
+    """
+    if 'state' in vals: # si cambia el estado
+      # si el estado es subsanación tiene que haber una razón
+      if vals['state'] == '1' and self._check_attribute_value('correction_reason', vals): 
+        raise ValidationError(f'La convalidación de {self.subject_id.name} tiene un estado de subsanación y no se ha definido la razón')
+
+      # si el estado es instancia superior tiene que haber un comentario  
+      if vals['state'] == '2' and self._check_attribute_value('comment', vals): 
+        raise ValidationError(f'La convalidación de {self.subject_id.name} se ha escalado a un instancia superior y no se ha definido un comentario justificándolo')
+      
+      if int(vals['state']) > 2 and \
+        (self._check_attribute_value('mark', vals) or \
+         self._check_attribute_value('validation_reason', vals) or \
+         self._check_attribute_value('validation_type', vals) or \
+         self._check_attribute_value('accepted', vals) or \
+         self._check_attribute_value('comment', vals)):
+          raise ValidationError(f'La convalidación de {self.subject_id.name} no ha definido la nota y/o la razón y/o un comentario')
+      
+      # si no es subsanación se elimina la razon
+      if vals['state'] != '1':
+        vals['correction_reason'] = False
+
+    else:  
+      # si el estado es subsanación tiene que haber una razón
+      if self.state == '1' and self._check_attribute_value('correction_reason', vals): 
+        raise ValidationError(f'La convalidación de {self.subject_id.name} tiene un estado de subsanación y no se ha definido la razón')
+      
+      # si el estado es instancia superior tiene que haber un comentario  
+      if self.state == '2' and self._check_attribute_value('comment', vals): 
+        raise ValidationError(f'La convalidación de {self.subject_id.name} se ha escalado a un instancia superior y no se ha definido un comentario justificándolo')
+   
+      if int(self.state) > 2 and \
+        (self._check_attribute_value('mark', vals) or \
+         self._check_attribute_value('validation_reason', vals) or \
+         self._check_attribute_value('validation_type', vals) or \
+         self._check_attribute_value('accepted', vals) or \
+         self._check_attribute_value('comment', vals)):
+          raise ValidationError(f'La convalidación de {self.subject_id.name} no ha definido la nota y/o la razón y/o un comentario')
+
+    return super(ValidationSubject, self).write(vals)
   
   def _create_validations(self):
     validations_path = self.env['res.config_parameter'].sudo().get_param('validation_path') or None
